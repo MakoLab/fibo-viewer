2de358978170f4a2914eae68ec70333a71619f80
Michał Daniel <michal.daniel@makolab.com>
2019-08-29 13:49:06 +0200

    Code analysis

--- weasel/src/main/java/org/edmcouncil/spec/fibo/weasel/ontology/WeaselOntologyManager.java
+++ weasel/src/main/java/org/edmcouncil/spec/fibo/weasel/ontology/WeaselOntologyManager.java
@@ -1,30 +1,16 @@
 package org.edmcouncil.spec.fibo.weasel.ontology;
 
-import com.sun.scenario.effect.Merge;
 import java.io.File;
-import java.io.FileOutputStream;
 import org.edmcouncil.spec.fibo.config.utils.files.FileSystemManager;
 import org.edmcouncil.spec.fibo.weasel.model.OwlDetails;
 import java.io.IOException;
-import java.io.OutputStream;
-import static java.lang.System.load;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
-import static java.util.ServiceLoader.load;
-import static java.util.ServiceLoader.load;
-import static java.util.ServiceLoader.load;
-import static java.util.ServiceLoader.load;
 import java.util.Set;
-import java.util.logging.Level;
 import java.util.stream.Stream;
-import static javafx.fxml.FXMLLoader.load;
-import static javafx.fxml.FXMLLoader.load;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 import org.edmcouncil.spec.fibo.config.configuration.model.AppConfiguration;
@@ -37,14 +23,9 @@ import org.edmcouncil.spec.fibo.weasel.model.PropertyValue;
 import org.edmcouncil.spec.fibo.weasel.ontology.data.OwlDataHandler;
 import org.semanticweb.owlapi.apibinding.OWLManager;
 import org.semanticweb.owlapi.model.IRI;
-import org.semanticweb.owlapi.model.MissingImportHandlingStrategy;
-import org.semanticweb.owlapi.model.OWLDataFactory;
-import org.semanticweb.owlapi.model.OWLImportsDeclaration;
 import org.semanticweb.owlapi.model.OWLOntology;
 import org.semanticweb.owlapi.model.OWLOntologyCreationException;
-import org.semanticweb.owlapi.model.OWLOntologyLoaderConfiguration;
 import org.semanticweb.owlapi.model.OWLOntologyManager;
-import org.semanticweb.owlapi.model.OWLOntologyStorageException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -82,8 +63,7 @@ public class WeaselOntologyManager {
   }
 
   private void loadOntologyFromFile() throws IOException, OWLOntologyCreationException {
-    OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
-
+   
     FileSystemManager fsm = new FileSystemManager();
     File inputOntologyFile = fsm.getPathToOntologyFile().toFile();
 
@@ -184,7 +164,6 @@ public class WeaselOntologyManager {
       for (Map.Entry<String, List<PropertyValue>> entry : owlDetails.getProperties().entrySet()) {
         String propertyKey = entry.getKey();
         String groupName = null;
-        String groupSubClassOf = null;
         groupName = getGroupName(groups, propertyKey);
         groupName = groupName == null ? DEFAULT_GROUP_NAME : groupName;
         for (PropertyValue property : entry.getValue()) {
